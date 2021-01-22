720decb8ff766c93d80e553bb4177955ffe0b006
Matt McDonald <mcdonamj087@gmail.com>
2021-01-21 20:01:02 -0600

    Fixed accordion indicators being out of line

--- gatsby/src/templates/service-learn-page/service-learn-page.template.js
+++ gatsby/src/templates/service-learn-page/service-learn-page.template.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../../components/layout';
 import Header from '../../components/header/header.component';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import Image from 'gatsby-image';
 import ScrollArrow from '../../components/scroll-arrow/scroll-arrow.component';
 import { Controller, Scene } from 'react-scrollmagic';
